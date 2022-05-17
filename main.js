7d50754336ab6597e4f518c24450a16e0ff9a2e6
shimonzhion <brys94204@gmail.com>
2022-05-17 16:12:32 +0300

    15.05.22

---
+++ main.js
@@ -0,0 +1,53 @@
+// function sum(x, y) {
+//   if (x > y) {
+//     for (var i = x; i >= y; i--) {
+//       console.log(i);
+//     }
+//   } else if (y > x) {
+//     for (var i = x; i < y; i++) {
+//       console.log(i);
+//     }
+//   }
+// }
+// var max = 0;
+// var min =[]
+// var i=0;
+// var numbers =0
+// while(i<5){
+//     numbers=+prompt("enter number")
+//     if (max<numbers){
+//         max = numbers
+//     }
+//     // else if (max>numbers){
+//      min.push(numbers)
+//     // }
+//     i++
+// }
+// console.log(max, Math.min(...min))
+// var max = 0;
+// var min = Number.MAX_SAFE_INTEGER;
+// var i=0;
+// var numbers =0
+// while(i<5){
+//     numbers=+prompt("enter number")
+//     if (max<numbers){
+//         max = numbers
+//     }
+// else{
+//      min = numbers
+// }
+//     i++
+// }
+// console.log(max,min)
+
+// function array1 (array){
+//     var max =0;
+//     for (var i =0; i <array.length;i++){
+//         if (max < array[i]){
+//             max = array[i]
+//         }
+
+//     }
+//     console.log(max)
+// }
+// array1([9,100,45,314,5])
